b2d5e672388c7cc8a66a1961fa04068857f0a53d
andrew.leach <type3dude@gmail.com>
2016-12-01 11:30:12 -0800

    Stubbed out two packages and classes for future work

---
+++ jdbp-master/src/driver/ResourceLocator.java
@@ -0,0 +1,10 @@
+package driver;
+
+public class ResourceLocator {
+
+	public static boolean locateJDBCDriver() {
+		// TODO locate the jdbc driver
+		// return success or fail
+		return false;
+	}
+}
---
+++ jdbp-master/src/parser/ResultSetTransposer.java
@@ -0,0 +1,6 @@
+package parser;
+
+public class ResultSetTransposer {
+
+	// methods to change the result set into the requested objects
+}
